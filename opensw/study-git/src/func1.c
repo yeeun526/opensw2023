4765ed35fc3a3a74abf0a366731cd2c3808baae2
Park Yeeun <yeeun05261@gmail.com>
2023-11-09 16:17:30 +0900

    apple() function is changed (for loop)

--- opensw/study-git/src/func1.c
+++ opensw/study-git/src/func1.c
@@ -1,7 +1,11 @@
 #include "funcs.h"
 
-void apple(){
+void apple(int number){
     printf("apple is delicious~!");
     printf("apple is great!");
+
+    for(int i=0;i<number;i++){
+        printf("apple %d \t",i);
+    }
     return;
 }
